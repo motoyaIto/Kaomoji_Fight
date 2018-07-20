6cc7a2001a16ba188718f9d5ba3e501084451168
HiromuHayasi <galaxy68xxx@gmail.com>
2018-07-20 16:43:51 +0900

    【プレイヤー作成】途中だけど

---
+++ Kaomoji_Fight/Assets/#script/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region
+    // 非公開
+    private Rigidbody2D rgb2d;
+
+    // 公開
+    public float speed = 3;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        rgb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 右・左
+        float x = Input.GetAxisRaw("Horizontal");
+
+        // 上・下
+        float y = Input.GetAxisRaw("Vertical");
+
+        // 移動する向きを求める
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        // 移動
+        rgb2d.velocity = direction * speed;
+	}
+}
