aaa573de6f395f59c861c07f62ca0be55ad8a066
motoya <hubuki008@gmail.com>
2018-10-10 19:26:51 +0900

    [scene]プレイヤーのHPの名前の表示を変更

--- Kaomoji_Fight/Assets/#script/Manager/PlaySceneManager.cs
+++ Kaomoji_Fight/Assets/#script/Manager/PlaySceneManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Windows;
 using XboxCtrlrInput;
-using System.Collections;
+using TMPro;
 
 
 public class PlaySceneManager : MonoBehaviour
@@ -12,6 +12,15 @@ public class PlaySceneManager : MonoBehaviour
   
     [SerializeField]
     private GameObject UICanvases;      //UI用キャンバス
+
+    [SerializeField]
+    private Color P1_nameColor = new Color(0.000f, 0.000f, 0.000f);
+    [SerializeField]
+    private Color P2_nameColor = new Color(0.000f, 0.000f, 0.000f);
+    [SerializeField]
+    private Color P3_nameColor = new Color(0.000f, 0.000f, 0.000f);
+    [SerializeField]
+    private Color P4_nameColor = new Color(0.000f, 0.000f, 0.000f);
 
     private GameObject[] player_textuer;    //各プレイヤーの画像
 
@@ -41,8 +50,7 @@ public class PlaySceneManager : MonoBehaviour
             }
 
             //プレイヤーとHPバーを生成
-            CreatePlayer(players[i], i);
-            CreateHPgage(HPgage[i], i);
+            this.CreatePlayer(players[i],HPgage[i], i);
         }
        
     }
@@ -58,7 +66,7 @@ public class PlaySceneManager : MonoBehaviour
     /// </summary>
     /// <param name="player">プレイヤーオブジェクトデータ</param>
     /// <param name="i">何番目のプレイヤーか</param>
-    private void CreatePlayer(GameObject player, int i)
+    private void CreatePlayer(GameObject player, GameObject HPgage, int i)
     {
         //ステージごとにリスポンする位置を調整する必要性あり
         switch (i)
@@ -67,24 +75,28 @@ public class PlaySceneManager : MonoBehaviour
                 GameObject P1 = Instantiate(player, new Vector3(2.5f, 50.0f, 0.0f), Quaternion.identity);
 
                 this.SetPlayerStatus(P1, XboxController.First, "P1");
+                this.CreateHPgage(HPgage,P1.name, i);
                 break;
 
             case 1:
                 GameObject P2 = Instantiate(player, new Vector3(15.5f, 50.0f, 0.0f), Quaternion.identity);
 
                 this.SetPlayerStatus(P2, XboxController.Second, "P2");
+                this.CreateHPgage(HPgage, P2.name, i);
                 break;
 
             case 2:
-                GameObject P3 = Instantiate(player, new Vector3(2.5f, 50.0f, 0.0f), Quaternion.identity);
+                GameObject P3 = Instantiate(player, new Vector3(15.5f, 50.0f, 0.0f), Quaternion.identity);
 
                 this.SetPlayerStatus(P3, XboxController.Third, "P3");
+                this.CreateHPgage(HPgage, P3.name, i);
                 break;
 
             case 3:
                 GameObject P4 = Instantiate(player, new Vector3(2.5f, 50.0f, 0.0f), Quaternion.identity);
 
                 this.SetPlayerStatus(P4, XboxController.Fourth, "P4");
+                this.CreateHPgage(HPgage, P4.name, i);
                 break;
         }
     } 
@@ -124,7 +136,7 @@ public class PlaySceneManager : MonoBehaviour
     /// </summary>
     /// <param name="HPgage">HPゲージ</param>
     /// <param name="i">何番目か</param>
-    private void CreateHPgage(GameObject HPgage, int i)
+    private void CreateHPgage(GameObject HPgage, string name, int i)
     {
         RectTransform size = HPgage.GetComponent<RectTransform>();
 
@@ -134,30 +146,53 @@ public class PlaySceneManager : MonoBehaviour
                 GameObject P1_HPgage = Instantiate(HPgage, new Vector3(size.sizeDelta.x / 2, Screen.height - 10, 0f), Quaternion.identity, UICanvases.transform);
 
                 P1_HPgage.name = "P1_HPgage";
+
+                //名前の設定
+                TextMeshProUGUI P1name = P1_HPgage.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+                P1name.text = name;
+                P1name.color = P1_nameColor;
+
                 break;
 
             case 1:
                 GameObject P2_HPgage = Instantiate(HPgage, new Vector3(Screen.width - size.sizeDelta.x / 2, Screen.height - 10, 0), Quaternion.identity, UICanvases.transform);
 
                 P2_HPgage.name = "P2_HPgage";
+
+                //名前の設定
+                TextMeshProUGUI P2name = P2_HPgage.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+                P2name.text = name;
+                P2name.color = P2_nameColor;
                 break;
 
             case 2:
                 GameObject P3_HPgage = Instantiate(HPgage, new Vector3(size.sizeDelta.x / 2, 10, 0), Quaternion.identity, UICanvases.transform);
 
                 P3_HPgage.name = "P3_HPgage";
+
+                //名前の設定
+                TextMeshProUGUI P3name = P3_HPgage.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+                P3name.text = name;
+                P3name.color = P3_nameColor;
                 break;
 
             case 3:
                 GameObject P4_HPgage = Instantiate(HPgage, new Vector3(Screen.width - size.sizeDelta.x / 2, 10, 0), Quaternion.identity, UICanvases.transform);
 
                 P4_HPgage.name = "P4_HPgage";
+
+                //名前の設定
+                TextMeshProUGUI P4name = P4_HPgage.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+                P4name.text = name;
+                P4name.color = P4_nameColor;
                 break;
         }
     }
     
 
-    //各プレイヤーの画像
+    /// <summary>
+    /// 各プレイヤーの画像
+    /// </summary>
     public GameObject[] Player_textuer
     {
         set
