7e20413e7f4c1b1e99c1f9872ae959fbf0d23da9
motoya <hubuki008@gmail.com>
2018-10-10 09:54:31 +0900

    [scene]プレイ人数の値の管理を変更

---
+++ Kaomoji_Fight/Assets/#script/PlayersData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersData
+{
+    private static readonly int PLAYERMAX = 4;//プレイヤーの最大人数
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="players">プレイヤーの人数</param>
+    public PlayersData(int players = 1)
+    {
+        Instance = this;
+        playerNum = players;
+
+        //プレイヤーが最大値を超えて生成しようとしたときに抑える
+        if (playerNum > PLAYERMAX)
+        {
+            playerNum = PLAYERMAX;
+        }
+    }
+
+    public static PlayersData Instance
+    {
+        get;
+        private set;
+    }
+
+    public int playerNum = 1;
+    
+}
