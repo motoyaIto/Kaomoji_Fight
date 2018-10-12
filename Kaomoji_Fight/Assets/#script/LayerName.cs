b4c22659d5e8440338abdf8e9e16466774319319
HiromuHayasi <galaxy68xxx@gmail.com>
2018-10-12 20:10:06 +0900

    Player改造中

---
+++ Kaomoji_Fight/Assets/#script/LayerName.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// レイヤー名を定数で管理するクラス
+/// </summary>
+public static class LayerName
+{
+	public const int Default = 0;
+	public const int TransparentFX = 1;
+	public const int IgnoreRaycast = 2;
+	public const int Water = 4;
+	public const int UI = 5;
+	public const int Obstacle = 8;
+	public const int Player = 9;
+	public const int BG = 10;
+	public const int Through = 11;
+	public const int DefaultMask = 1;
+	public const int TransparentFXMask = 2;
+	public const int IgnoreRaycastMask = 4;
+	public const int WaterMask = 16;
+	public const int UIMask = 32;
+	public const int ObstacleMask = 256;
+	public const int PlayerMask = 512;
+	public const int BGMask = 1024;
+	public const int ThroughMask = 2048;
+}
--- Kaomoji_Fight/Assets/#script/Stage/BlockController.cs
+++ Kaomoji_Fight/Assets/#script/Stage/BlockController.cs
@@ -8,12 +8,19 @@ public class BlockController : MonoBehaviour {
     private float ResetTime = 10.0f;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        colliderOfPass = GetComponent<BoxCollider2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-      
+        if (setPass)
+        {
+            colliderOfPass.enabled = false;
+        }
+        if (!setPass)
+        {
+            colliderOfPass.enabled = true;
+        }
     }
 
     private void OnDisable()
@@ -31,5 +38,27 @@ public class BlockController : MonoBehaviour {
     public void ChangeWeapon()
     {
         this.gameObject.SetActive(false);
+    }
+
+    bool setPass;
+    BoxCollider2D colliderOfPass;
+
+
+
+    //プレイヤーのIsTriggerがOnの側のコリジョンが床のIsTriggerがOnの側のコリジョンと接触している時
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            setPass = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            setPass = false;
+        }
     }
 }
