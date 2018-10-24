467d7776cd0a68dba2520373505bc0092eb5a094
HiromuHayasi <galaxy68xxx@gmail.com>
2018-10-24 15:33:48 +0900

    Player は 「ぶつり」を おぼえた！

--- Kaomoji_Fight/Assets/#scene/Test/MoveScript1.cs
+++ Kaomoji_Fight/Assets/#scene/Test/MoveScript1.cs
@@ -4,64 +4,6 @@ using UnityEngine;
 
 public class MoveScript1 : MonoBehaviour {
 
-    //[Header("移動速度に対する追従度")]
-    //public float moveForceMultiplier = 0f;
-    //[Header("移動速度")]
-    //public float moveSpeed = 10f;
-
-    //private Rigidbody2D rbody;
-    //private AudioSource audio;
-    //private float XaxisInput;
-    //private float YaxisInput;
-
-    //// Use this for initialization
-    //void Start()
-    //{
-    //    rbody = GetComponent<Rigidbody2D>();
-    //    audio = GetComponent<AudioSource>();
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    XaxisInput = Input.GetAxis("Horizontal");
-    //    YaxisInput = Input.GetAxis("Vertical");
-    //}
-
-    //private void FixedUpdate()
-    //{
-    //    Vector2 movevector = Vector2.zero;
-
-
-    //    if (XaxisInput != .0f)
-    //    {
-    //        movevector.x = moveSpeed * XaxisInput;
-    //    }
-    //    else
-    //    {
-    //        movevector.x = .0f;
-    //    }
-
-    //    if (YaxisInput != .0f)
-    //    {
-    //        movevector.y = moveSpeed * YaxisInput;
-    //    }
-    //    else
-    //    {
-    //        movevector.y = .0f;
-    //    }
-
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        rbody.AddForce(transform.up * 10f);
-    //    }
-
-    //    rbody.AddForce(moveForceMultiplier * (movevector - rbody.velocity));
-
-    //}
-
-
-    //変数定義
     public float flap = 1000f;
     public float scroll = 5f;
     float direction = 0f;
@@ -102,17 +44,17 @@ public class MoveScript1 : MonoBehaviour {
         if (Input.GetKeyDown("space") && !jump)
         {
             rb2d.AddForce(Vector2.up * flap);
-            jump = true;
+            //jump = true;
         }
 
 
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Weapon"))
-        {
-            jump = false;
-        }
-    }
+    //void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Weapon"))
+    //    {
+    //        jump = false;
+    //    }
+    //}
 }
