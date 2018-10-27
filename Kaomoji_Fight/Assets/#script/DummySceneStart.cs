2526be4b9f9c1a9a8c75ae556f3469834e6e4e98
motoya <hubuki008@gmail.com>
2018-10-28 01:23:09 +0900

    [SelectSceneManager]仮）シーン切替完了

--- Kaomoji_Fight/Assets/#script/DummySceneStart.cs
+++ Kaomoji_Fight/Assets/#script/DummySceneStart.cs
@@ -8,10 +8,10 @@ public class DummySceneStart : MonoBehaviour {
     private PlayData playedata;//プレイデータ
 
     [SerializeField, Header("選択したステージ名(.txt記述しなくて良い)（デバッグ用）")]
-    private string textmame = "stage1";
+    private string Stage_name = "stage1";
 
     [SerializeField, Header("プレイヤーの名前(デバッグ用)")]
-    private string[] playersName = { "P1", "P2", "P3", "P4"};
+    private string[] players_name = { "P1", "P2", "P3", "P4"};
     [SerializeField, Header("プレイヤーの顔（デバッグ用）")]
     Sprite[] playersface;
    
@@ -19,7 +19,7 @@ public class DummySceneStart : MonoBehaviour {
     {
         //string DammySelectStage =textmame;
 
-        playedata = new PlayData(textmame, playersName, playersface);
+        //playedata = new PlayData(Stage_name, players_name, playersface);
     }
 
     // Use this for initialization
