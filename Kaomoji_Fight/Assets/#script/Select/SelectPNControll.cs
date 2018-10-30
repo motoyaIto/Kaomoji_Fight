326cb7c350bd224531b8e8874a42aac54673c3bf
Rot331 <kontomo0331@gmail.com>
2018-10-30 14:04:54 +0900

    カーソル修正

--- Kaomoji_Fight/Assets/#script/Select/SelectPNControll.cs
+++ Kaomoji_Fight/Assets/#script/Select/SelectPNControll.cs
@@ -15,7 +15,7 @@ public class SelectPNControll : MonoBehaviour {
     private XboxController ControlerNamber = XboxController.First;//何番目のコントローラーを適用するか
 
     bool cursor = true;
-    bool move = true;
+    bool move = false;
     private static readonly int PLAYERMAX = 4;
     public int PlayerNum = 1;
     private AudioSource sound01;
@@ -34,105 +34,75 @@ public class SelectPNControll : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine("Select");
-    }
-
-    IEnumerator Select()
-    {
         Transform myTransform = this.transform;
         Vector3 pos = myTransform.position;
 
         // Controllerの左スティックのAxisを取得            
         Vector2 input = new Vector2(XCI.GetAxis(XboxAxis.LeftStickX, ControlerNamber), XCI.GetAxis(XboxAxis.LeftStickY, ControlerNamber));
-
-        if(input.y<-0.9f||input.y>0.9f)
+        
+        if (input.y == 0)
         {
-            move = false;
+            //スティックを倒してなかったらtrueに
+            move = true;
         }
         if (cursor == true)
         {
-            if (Input.GetKeyDown(KeyCode.DownArrow) || XCI.GetDPadDown(XboxDPad.Down, ControlerNamber) || input.y < -0.9f)
+            if (move == true)
             {
-                if(move==true)
+                //下を押したときの処理
+                if (Input.GetKeyDown(KeyCode.DownArrow) || XCI.GetDPadDown(XboxDPad.Down, ControlerNamber) || input.y < -0.9f)
                 {
                     if (sound01.isPlaying == false)
                     {
                         sound01.PlayOneShot(sound01.clip);
+                        //プレイヤーの合計人数
+                        PlayerNum++;
+                        //4の時に下を押したら1に戻る
+                        if (PlayerNum > PLAYERMAX) 
+                        {
+                            PlayerNum = 1;
+                        }
                     }
+                    //4の時に下を押したら1に戻る
                     if (pos.y <= -3.3f)
                     {
                         pos.y = 2.2f;
                     }
+                    //カーソル移動
                     else
                     {
                         pos.y -= 1.9f;
                     }
-                }
-                if(move==false)
-                {
-                    yield return new WaitForSeconds(0.1f); // num秒待機                    
-                    if (pos.y <= -3.3f)
-                    {
-                        pos.y = 2.2f;
-                    }
-                    else
-                    {
-                        pos.y -= 1.9f;
-                    }
-                    if (sound01.isPlaying == false)
-                    {
-                        sound01.PlayOneShot(sound01.clip);
-                    }
-                }
-                //プレイヤーの合計人数
-                PlayerNum++;
 
-                if (PlayerNum > PLAYERMAX)
-                {
-                    PlayerNum = 1;
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.UpArrow) || XCI.GetDPadDown(XboxDPad.Up, ControlerNamber) || input.y > 0.9f)
-            {
-                if (move == true)
+                    Debug.Log("+して" + PlayerNum);
+                    move = false;
+                }           
+                //上を押したときの処理
+                if (Input.GetKeyDown(KeyCode.UpArrow) || XCI.GetDPadDown(XboxDPad.Up, ControlerNamber) || input.y > 0.9f)
                 {
                     if (sound01.isPlaying == false)
                     {
+                        //プレイヤーの合計人数
+                        PlayerNum--;
+                        //1の時に上を押したら4に行く
+                        if (PlayerNum < 1)
+                        {
+                            PlayerNum = PLAYERMAX;
+                        }
                         sound01.PlayOneShot(sound01.clip);
                     }
-                    if (pos.y >= 2.0f)
+                    //1の時に上上を押したら4に行く                
+                    if (pos.y >= 2.0f)   
                     {
                         pos.y = -3.5f;
                     }
+                    //カーソルを上に移動
                     else
                     {
                         pos.y += 1.9f;
                     }
-                }
-
-                if (move == false)
-                {
-                    yield return new WaitForSeconds(0.1f); // num秒待機                    
-                    if (pos.y >= 2.0f)
-                    {
-                        pos.y = -3.5f;
-                    }
-                    else
-                    {
-                        pos.y += 1.9f;
-                    }
-                    if (sound01.isPlaying == false)
-                    {
-                        sound01.PlayOneShot(sound01.clip);
-                    }
-                }
-
-                //プレイヤーの合計人数
-                PlayerNum--;
-
-                if (PlayerNum < 1)
-                {
-                    PlayerNum = PLAYERMAX;
+                    Debug.Log("-して" + PlayerNum);
+                    move = false;
                 }
             }
             myTransform.position = pos;  // 座標を設定
@@ -141,12 +111,14 @@ public class SelectPNControll : MonoBehaviour {
             {
                 SelectSceneManager SSManager_script = SSManager.GetComponent<SelectSceneManager>();
 
-                SSManager_script.PlayerNam_Data = PlayerNum;
+                SSManager_script.PlayerNam_Data = PlayerNum;                
                 sound01.PlayOneShot(sound02.clip);
                 cursor = false;
-                camera.transform.position += new Vector3(17.8f, 0, 0);
+                camera.transform.position += new Vector3(17.8f, 0, 0);  //ステージセレクトに移動
+                Debug.Log("最終値" + PlayerNum);
             }
         }
+        //ステージセレクトから戻った時の処理
         if (Input.GetKeyDown(KeyCode.Backspace) || XCI.GetButtonDown(XboxButton.A, ControlerNamber))
         {
             if (cursor == false)
@@ -156,5 +128,5 @@ public class SelectPNControll : MonoBehaviour {
             }
 
         }
-    }
+    }   
 }
