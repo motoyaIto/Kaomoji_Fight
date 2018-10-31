b925af2d9784d3bdb91c3f352cbbe91f1df827a5
motoya <hubuki008@gmail.com>
2018-10-31 11:34:43 +0900

    [PlaySceneTimer]制作中

--- Kaomoji_Fight/Assets/#script/PlayTimer.cs
+++ Kaomoji_Fight/Assets/#script/PlayTimer.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 public class PlayTimer : MonoBehaviour {
 
+    [SerializeField]
+    private float countTime = 180;
+    private float nowTime = 0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        //Observalbe.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1))
+        //    .Select(coutTime => (int)(coutTime - coutTime));
 	}
 	
 	// Update is called once per frame
